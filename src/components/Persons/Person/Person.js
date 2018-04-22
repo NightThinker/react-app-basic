eb1a7f7a59e8b8a5a8827842d79455c8dfba967c
jiraphon <jiraphon@sirimedia.com>
2018-04-22 20:06:29 +0700

    prop-types validation

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import withClass from '../../../hoc/withClass';
 import Aux from '../../../hoc/Aux';
@@ -30,5 +31,12 @@ class Person  extends Component {
         )
     }
 }
+
+Person.propTypes = {
+    click: PropTypes.func,
+    name: PropTypes.string,
+    age: PropTypes.number,
+    changed: PropTypes.func
+};
 
 export default withClass(Person, classes.Person);
